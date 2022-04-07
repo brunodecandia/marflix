a0d564cafae577198161012483942905364726c7
Bruno <brunodecandia@gmail.com>
2022-04-06 23:14:59 -0300

    Iniciando repositorio da primeira versao

---
+++ js/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        350:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:9
+        }
+    }
+})
